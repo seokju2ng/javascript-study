83058487312af7bcb881bb5e9b5a1b680f6c6bc2
seokju2ng <ehddnwnd@naver.com>
2020-07-22 23:57:50 +0900

    added chapter 02

--- chapter02/grammer.js
+++ chapter02/grammer.js
@@ -2,3 +2,23 @@ console.log('Hello JavaScript');
 console.log(52.273);
 console.log(273);
 console.log(30 > 20 > 10);
+
+window.onload = function() {
+  var list = '';
+  list += '<ul>';
+  list += ' <li>Hello</li>';
+  list += ' <li>JavaScript</li>';
+  list += '</ul>';
+
+  document.body.innerHTML = list;
+}
+
+
+var input = prompt('Message', 'DefStr');
+
+console.log(input);
+
+
+input = confirm('yes or no');
+
+console.log(input);
